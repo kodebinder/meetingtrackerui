a96d55c2397acf7d9c4dae22d47d2e185e80a8fb
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2022-04-22 21:08:39 +0530

    added feedback in menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './component/error/error.component';
+import { FeedbackComponent } from './component/feedback/feedback.component';
 import { ListMeetingsComponent } from './component/list-meetings/list-meetings.component';
 import { LoginComponent } from './component/login/login.component';
 import { LogoutComponent } from './component/logout/logout.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'welcome/:username', component: WelcomeComponent },
   { path: 'meetings', component: ListMeetingsComponent },
+  { path: 'feedback', component: FeedbackComponent },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }
 ];
