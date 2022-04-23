8e34520cdd28261596d2dc3e0b84c5c8d693bd95
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2022-04-23 09:25:44 +0530

    feedback form v1

--- src/app/component/feedback/feedback.component.ts
+++ src/app/component/feedback/feedback.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomvalidationService } from 'src/app/service/customvalidation.service';
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required], this.customValidator.userNameValidator.bind(this.customValidator)],
+      password: ['', Validators.compose([Validators.required])],
+      confirmPassword: ['', [Validators.required]],
+    },
+      {
+        validator: this.customValidator.MatchPassword('password', 'confirmPassword'),
+      }
+    );
   }
 
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.registerForm.value);
+    }
+  }
 }
